f3c9b6e047f4f455a111cb4907408805f810aee1
Moosie99 <emmah@kirkireton.com>
2024-09-19 13:29:36 +0100

    If statements chapter

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+C:\Users\emmah\OneDrive\Documents\Projects\Rust\Rust\branches\target\debug\branches.exe: C:\Users\emmah\OneDrive\Documents\Projects\Rust\Rust\branches\src\main.rs
---
+++ branches/target/debug/deps/branches.d
@@ -0,0 +1,5 @@
+C:\Users\emmah\OneDrive\Documents\Projects\Rust\Rust\branches\target\debug\deps\branches.exe: src/main.rs
+
+C:\Users\emmah\OneDrive\Documents\Projects\Rust\Rust\branches\target\debug\deps\branches.d: src/main.rs
+
+src/main.rs:
